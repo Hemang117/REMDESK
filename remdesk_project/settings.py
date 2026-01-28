ea5c6f8ac716f3bd9bf1cae7323790f1ff587ee8
Hemang117 <hemangsaini46600@gmail.com>
2026-01-28 13:00:29 +0530

    Allow remdeskjobs.com in settings

--- remdesk_project/settings.py
+++ remdesk_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-dev-only'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') + ['.vercel.app', 'now.sh']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') + ['.vercel.app', 'now.sh', 'remdeskjobs.com', 'www.remdeskjobs.com']
 
 
 # Application definition
