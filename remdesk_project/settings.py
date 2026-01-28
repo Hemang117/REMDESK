ce7e762b36ce52cc4cf79ae6464094300e24ad35
Hemang117 <hemangsaini46600@gmail.com>
2026-01-28 12:26:32 +0530

    Configure Whitenoise StaticFilesStorage

--- remdesk_project/settings.py
+++ remdesk_project/settings.py
@@ -161,6 +161,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Whitenoise Storage (Compression & Caching)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
