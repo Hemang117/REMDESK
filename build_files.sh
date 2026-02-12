501d9e3b4c8a2183cd83f398e4cb1a38758ef3fc
Hemang117 <hemangsaini46600@gmail.com>
2026-02-12 13:50:06 +0530

    fix: make build_files.sh pip install resilient for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
 
 echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt --break-system-packages
-
-# Migrations should be run manually or via a separate release command
-# python3 manage.py migrate
+pip install -r requirements.txt --no-cache-dir --break-system-packages || \
+pip install -r requirements.txt --no-cache-dir || \
+python3 -m pip install -r requirements.txt --no-cache-dir
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
