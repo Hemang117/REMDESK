d830778829b552b8bf04af355403450f6f712b2b
Hemang117 <hemangsaini46600@gmail.com>
2026-02-12 13:53:17 +0530

    fix: remove future package (six.moves conflict) + mkdir staticfiles safety net

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt --no-cache-dir || \
 python3 -m pip install -r requirements.txt --no-cache-dir
 
 echo "Collecting static files..."
+mkdir -p staticfiles
 python3 manage.py collectstatic --noinput
 
 echo "Done!"
